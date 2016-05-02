a8dd4337de5dd6a9bb1e07c159eea4f88b828436
jb2718 <jbattle2718@gmail.com>
2016-05-02 05:04:29 -0400

    complete exercises in variables chapter

---
+++ variables/ex5.rb
@@ -0,0 +1,4 @@
+# In the first program, x = 3 at the end of the program.
+# In the second program, an error should be thrown because
+# the x variable was defined within the scope of the method
+# "times".  Therefore, it is not defined outside that method
---
+++ variables/ex6.rb
@@ -0,0 +1,4 @@
+# This error message tells me that the user tried to access
+# a variable or method named "shoes" in irb that was not defined
+# in the scope in which it was called/used.  It also
+# indicates the line on which the error occured
